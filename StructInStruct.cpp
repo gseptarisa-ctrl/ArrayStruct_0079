21991cb617dc9c8da1ac43157edc83cfd20e9021
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-07 15:19:36 +0700

    Menambahkan sekaligus menampilkan isi objek

--- StructInStruct.cpp
+++ StructInStruct.cpp
@@ -10,3 +10,26 @@ struct orang{
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    // Deklarasi obyek dari struct orang
+    orang mhs;
+
+    //isi objek
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Alamat Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    //Menampilkan Isi Obyek
+    cout << "Nama  : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Umur  : " << mhs.umur << endl;
+}
+    
