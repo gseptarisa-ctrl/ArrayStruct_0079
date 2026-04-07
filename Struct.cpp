ba8797dfadfd2d135f67e2f04492055dd822ab7d
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-04-07 15:11:35 +0700

    Membuat int main struct

--- Struct.cpp
+++ Struct.cpp
@@ -8,3 +8,24 @@ struct orang {
     int umur;
 };
 
+int main() {
+    // Deklarasi obyek dari struct orang
+    orang mhs;
+
+    // Isi objek lewat input user
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Asal : ";
+    cin >> mhs.alamat;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    // Menampilkan Isi Obyek
+    cout << "//Menampilkan Isi Obyek" << endl;
+    cout << "Nama   : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur   : " << mhs.umur << endl;
+
+    return 0;
+}
